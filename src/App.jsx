e515026532a1d3a0e92ac3f7de944c0265350bc6
Rodrigo Bueno <buenodevv@gmail.com>
2024-05-12 07:29:23 -0300

    feat: layout setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,16 @@ function App() {
       <div className="w-64 fixed">
         <SideNav selectedIndex={(value)=>console.log(value)} />
       </div>
-      <div className="ml-64">
-        Body
+      <div className="ml-64 grid grid-cols-1 md:grid-cols-6">
+        <div className="md:col-span-2 bg-green-300">
+          Control Panel
+        </div>
+        <div className="md:col-span-3 bg-red-300">
+          Preview
+        </div>
+        <div className="bg-blue-300">
+    Ads
+        </div>
       </div>
     </>
   )
