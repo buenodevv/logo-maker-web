0dd098d7768a3a935f0aeb6678ae2b263ec240ca
Rodrigo Bueno <64937739+buenodevv@users.noreply.github.com>
2024-05-24 05:35:34 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,11 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/png':'https://logoexpress.tubeguruji.com'
-    }
+    },
+    
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8000,
   },
 })
